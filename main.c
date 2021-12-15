2530403e492292cd83b59253d6f92bc645f1dfa0
Ramírez Moreno Diego Gerardo <90343027+252003@users.noreply.github.com>
2021-12-15 12:27:04 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,85 +1,85 @@
 #include"conversiones.h"
 
 int main(){
-    int f;
+    int o;
     long n;
-    char binario[100], hexa[100], oct[100];
+    char bin[100], hexa[100], oct[100];
     strcpy(hexa, "");
-    strcpy(binario, "");
+    strcpy(bin, "");
     strcpy(oct, "");
     do{
-        printf("Bienvenidos a la calculadora de converision numerica:");
-        printf("\n1.Convertir de numero binario a numero decimal.");
-        printf("\n2.Convertir de numero binario a numero hexadecimal.");
-        printf("\n3.Convertir de numero binario a numero octal.");
-        printf("\n4.Convertir de numero decimal a numero binario.");
-        printf("\n5.Convertir de numero decimal a numero hexadecimal.");
-        printf("\n6.Convertir de numero decimal a numero octal.");
-        printf("\n7.Convertir de numero hexadecimal a numero binario.");
-        printf("\n8.Convertir de numero hexadecimal a numero decimal.");
-        printf("\n9.Convertir de numero octal a numero binario.");
-        printf("\n10.Convertir de numero octal a numero decimal.");
+        printf("Calculadora de conversiones:");
+        printf("\n1.Convertir de binario a decimal.");
+        printf("\n2.Convertir de binario a hexadecimal.");
+        printf("\n3.Convertir de binario a octal.");
+        printf("\n4.Convertir de decimal a bin.");
+        printf("\n5.Convertir de decimal a hexadecimal.");
+        printf("\n6.Convertir de decimal a octal.");
+        printf("\n7.Convertir de hexadecimal a bin.");
+        printf("\n8.Convertir de hexadecimal a decimal.");
+        printf("\n9.Convertir de octal a bin.");
+        printf("\n10.Convertir de octal a decimal.");
         printf("\n11.Salir.");
-        printf("\n ¿Que conversion deseas?\n ");
-        scanf("%d", &f);
-        switch (f){
+        printf("\n ¿Que operacion deseas realizar?: ");
+        scanf("%d", &o);
+        switch (o){
             case 1:
-                printf("\nSeleccionaste convertir de numero binario a numero decimal");
-                printf("\nIngresa un numero: ");
-                scanf("%s", binario);
-                conv_bin_decimal(binario);
+                printf("\nHas seleccionado convertir de numero binario a  numerodecimal");
+                printf("\nIngresa tu numero: ");
+                scanf("%s", bin);
+                conv_bin_dec(bin);
                 break;
             case 2:
-                printf("\nSeleccionaste convertir de numero binario a numero hexadecimal.");
-                printf("\nIngresa un numero: ");
-                scanf("%s", binario);
-                conv_bin_hexa(binario);
+                printf("\nHas seleccionado convertir de numero binario a  numerohexadecimal.");
+                printf("\nIngresa tu numero: ");
+                scanf("%s", bin);
+                conv_bin_hexa(bin);
                 break;
             case 3:
-                printf("\nSeleccionaste convertir de numero binario a numero octal.");
-                printf("\nIngresa un numero: ");
-                scanf("%s", binario);
-                conv_bin_oct(binario);
+                printf("\nHas seleccionado convertir de numero binario a numero octal.");
+                printf("\nIngresa tu numero: ");
+                scanf("%s", bin);
+                conv_bin_oct(bin);
                 break;
             case 4:
-                printf("\nSeleccionaste convertir de numero decimal a numero binario.");
-                printf("\nIngresa un numero: ");
+                printf("\nHas seleccionado convertir de numero decimal a numero binario.");
+                printf("\nIngresa tu numero: ");
                 scanf("%ld", &n);
                 conv_dec_bin(n);
                 break;        
             case 5:
-                printf("\nSeleccionaste convertir de numero decimal a numero hexadecimal.");
-                printf("\nIngresa un numero: ");
+                printf("\nHas seleccionado convertir de numero decimal a numero hexadecimal.");
+                printf("\nIngresa tu numero: ");
                 scanf("%ld", &n);
                 conv_dec_hexa(n);
                 break; 
             case 6:
-                printf("\nSeleccionaste convertir de numero decimal a numero octal.");
-                printf("\nIngresa un numero: ");
+                printf("\nHas seleccionado convertir de numero decimal a numero octal.");
+                printf("\nIngresa tu numero: ");
                 scanf("%ld", &n);
                 conv_dec_oct(n);
                 break;
             case 7:
-                printf("\nSeleccionaste convertir de numero hexadecimal a numero binario.");
-                printf("\nIngresa un numero  : ");
+                printf("\nHas seleccionado convertir de numero hexadecimal a numero binario.");
+                printf("\nIngresa tu numero: ");
                 scanf("%s", hexa);
                 conv_hexa_bin(hexa);
                 break;
             case 8:
-                printf("\nSeleccionaste convertir de numero hexadecimal a numero decimal.");
-                printf("\nIngresa un numero: ");
+                printf("\nHas seleccionado convertir de  numero hexadecimal a numero decimal.");
+                printf("\nIngresa tu numero: ");
                 scanf("%s", hexa);
                 conv_hexa_dec(hexa);
                 break;
             case 9:
-                printf("\nSeleccionaste convertir de numero octal a numero binario.");
-                printf("\nIngresa un numero: ");
+                printf("\nHas seleccionado convertir de numero octal a numero binario.");
+                printf("\nIngresa tu numero: ");
                 scanf("%s", oct);
                 conv_oct_bin(oct);
                 break;
             case 10:
-                printf("\nSeleccionaste convertir de numero octal a numero decimal.");
-                printf("\nIngresa un numero: ");
+                printf("\nHas seleccionado convertir de numero octal a numero decimal.");
+                printf("\nIngresa tu numero: ");
                 scanf("%s", oct);
                 conv_oct_dec(oct);
                 break;                
@@ -88,6 +88,6 @@ int main(){
         }
         printf("\n");
         printf("\n");
-    }while(f!=11 && f>0 && f<11);
+    }while(o!=11 && o>0 && o<11);
     return 0;
 }
