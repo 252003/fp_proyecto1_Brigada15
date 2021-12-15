7648b45937f070ca5e457c8452be4741f44982d1
Ramírez Moreno Diego Gerardo <90343027+252003@users.noreply.github.com>
2021-12-15 12:24:34 -0600

    Update conversiones.h

--- conversiones.h
+++ conversiones.h
@@ -5,20 +5,23 @@
 #include<string.h>
 #include<stdlib.h>
 
-void conv_dec_bin(int num);
-void conv_bin_decimal(char binario[]);
+void reverse_change(char []);
+void reverse(char reversa[]);
 
-void conv_dec_oct(int num);
+void conv_dec_bin(int n);  
+void conv_bin_dec(char bin[]); 
+
+void conv_dec_oct(int n);  
 void conv_oct_dec(char oct[]);
 
-void conv_dec_hexa(int num);
+void conv_dec_hexa(int n); 
 void conv_hexa_dec(char hexa[]); 
 
 
-void conv_bin_oct(char binario[]); 
-void conv_oct_bin(char oct[]);
+void conv_bin_oct(char bin[]); 
+void conv_oct_bin(char oct[]); 
 
-void conv_bin_hexa(char binario[]);
-void conv_hexa_bin(char hexa[]);
+void conv_bin_hexa(char bin[]); 
+void conv_hexa_bin(char hexa[]); 
 
 #endif 
